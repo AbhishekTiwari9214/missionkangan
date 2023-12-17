a9c971889dd7c9958278168e8af3d38b431305a7
Abhishek Tiwari <abhishek.tiwari2@celebaltech.com>
2023-12-17 22:35:55 +0530

    fist commit on mission kangan

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+// Import necessary React components and CSS file
 
-function App() {
+import React, { useState, useEffect } from 'react';
+import './App.css'; // Make sure to have a CSS file for styling
+import Home from './Home'
+import productList from './products.json';
+
+const App = () => {
+  // Sample data for the product list
+
+  // State for controlling the current slide in the slideshow
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  // Function to handle next slide in the slideshow
+  const nextSlide = () => {
+    setCurrentSlide((prevSlide) => (prevSlide + 1) % productList.length);
+  };
+
+  // Automatically advance to the next slide every 3 seconds (adjust as needed)
+  useEffect(() => {
+    const interval = setInterval(nextSlide, 3000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      {/* Slideshow Header */}
+      <div className="slideshow-container">
+        {productList.map((product, index) => (
+          <div
+            key={product.id}
+            className={`slide ${index === currentSlide ? 'active' : ''}`}
+          >
+            <img src={product.url1} alt={`Product ${product.id}`} />
+          </div>
+        ))}
+      </div>
+
+      {/* Small Info Div */}
+      <div className="info-div">
+        {/* Add your website information here */}
+        
+        <p>Comming Soon</p>
+      </div>
+
+      {/* Product List with Mobile Pattern */}
+      <Home productList={productList} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/ProductModal.js
@@ -0,0 +1,85 @@
+
+
+import React, { useState, useEffect } from 'react';
+import Modal from 'react-modal';
+import './ProductModal.css';
+import BuyNowModal from './BuyNowModal';
+
+const ProductModal = ({ isOpen, onRequestClose, selectedProduct }) => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [currentImage, setCurrentImage] = useState("");
+
+  const closeModal = () => {
+    
+    setModalIsOpen(false);
+  };
+  const handleImageClick = (imageUrl) => {
+    setCurrentImage(imageUrl);
+  };
+  const handleOpenModal = () => {
+    setModalIsOpen(true);
+  }
+
+  useEffect(() => {
+    setCurrentImage(selectedProduct?.url1);
+  }, [isOpen])
+  return (
+
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      contentLabel="Product Details Modal"
+      className="product-modal"
+      overlayClassName="product-modal-overlay"
+    >
+
+      {selectedProduct && (
+        <div className="modal-content">
+          <img src={currentImage} alt={`Product ${selectedProduct.id}`} className="modal-image" />
+          <div className="gallery">
+            {selectedProduct.url1 && (
+              <img
+                src={selectedProduct.url1}
+                alt={`Product ${selectedProduct.id}`}
+                className="thumbnail-image"
+                onClick={() => handleImageClick(selectedProduct.url1)}
+              />
+            )}
+            {selectedProduct.url2 && (
+              <img
+                src={selectedProduct.url2}
+                alt={`Product ${selectedProduct.id}`}
+                className="thumbnail-image"
+                onClick={() => handleImageClick(selectedProduct.url2)}
+              />
+            )}
+            {selectedProduct.url3 && (
+              <img
+                src={selectedProduct.url3}
+                alt={`Product ${selectedProduct.id}`}
+                className="thumbnail-image"
+                onClick={() => handleImageClick(selectedProduct.url3)}
+              />
+            )}
+          </div>
+          <div className="product-details">
+            <h2>{selectedProduct.description}</h2>
+            <p>Price: ${selectedProduct.price}</p>
+            <p>Sizes: {selectedProduct.size.join(', ')}</p>
+            <p>{selectedProduct.available ? 'Available' : 'Not Available'}</p>
+          </div>
+          <button className='closemodal' onClick={onRequestClose}></button>
+          <button className='openmodal' onClick={handleOpenModal}>buy now</button>
+          <BuyNowModal 
+             isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        selectedProduct={selectedProduct}
+          />
+        </div>
+      )}
+    </Modal>
+  );
+};
+
+
+export default ProductModal;
